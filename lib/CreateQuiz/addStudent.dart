e6c8bb97c670519e32dd524964608248981c1ad7
Aakarshak Arora <46300387+aakarshakarora@users.noreply.github.com>
2021-04-09 22:39:51 +0530

    Update addStudent.dart

--- lib/CreateQuiz/addStudent.dart
+++ lib/CreateQuiz/addStudent.dart
@@ -3,12 +3,11 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:quiz_app/Dashboard/F_Dashboard/dashboardFaculty.dart';
 
-
-
 class AddStudent extends StatefulWidget {
   final String groupName;
+  final DocumentReference docRef;
 
-  AddStudent(this.groupName);
+  AddStudent(this.groupName, this.docRef);
 
   @override
   _AddStudentState createState() => _AddStudentState();
@@ -18,12 +17,12 @@ class _AddStudentState extends State<AddStudent> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final userId = FirebaseAuth.instance.currentUser.uid;
   User currentUser;
+  List studentGroup;
 
   @override
   void initState() {
     super.initState();
     currentUser = getCurrentUser();
-
   }
 
 //Get Current User
@@ -37,7 +36,7 @@ class _AddStudentState extends State<AddStudent> {
     return Scaffold(
         appBar: AppBar(
             backgroundColor: Colors.blue,
-            title: Text("Add Student in "+widget.groupName),
+            title: Text("Add Student in " + widget.groupName),
             leading: IconButton(
                 icon: Icon(Icons.arrow_back),
                 onPressed: () {
@@ -53,7 +52,6 @@ class _AddStudentState extends State<AddStudent> {
                 child: StreamBuilder(
                   stream: FirebaseFirestore.instance
                       .collection('Student')
-                  // .where("mentorEmailId", isNotEqualTo: mentorEmail)
                       .snapshots(),
                   builder: (ctx, opSnapshot) {
                     if (opSnapshot.connectionState == ConnectionState.waiting)
@@ -65,8 +63,25 @@ class _AddStudentState extends State<AddStudent> {
                     return ListView.builder(
                       itemCount: reqDocs.length,
                       itemBuilder: (ctx, index) {
-                        if (opSnapshot.hasData)
-                          return ViewDetails(reqDoc:reqDocs[index], userID:currentUser.uid,groupName:widget.groupName);
+                        if (opSnapshot.hasData) {
+                          if (reqDocs[index]
+                                  .get('GroupAdded')
+                                  .contains(widget.docRef.toString()) ==
+                              false) {
+                            return ViewDetails(
+                              reqDoc: reqDocs[index],
+                              userID: currentUser.uid,
+                              groupName: widget.groupName,
+                              docRef: widget.docRef,
+                            );
+                          } else
+                            {
+                              return Container(
+                                height: 0,
+                              );
+                          }
+                        }
+
                         return Container(
                           height: 0,
                         );
@@ -85,31 +100,19 @@ class ViewDetails extends StatefulWidget {
   final dynamic reqDoc;
   final String userID;
   final String groupName;
+  final DocumentReference docRef;
 
-  ViewDetails({this.reqDoc, this.userID,this.groupName});
+  ViewDetails({this.reqDoc, this.userID, this.groupName, this.docRef});
 
   @override
   _ViewDetailsState createState() => _ViewDetailsState();
 }
 
 class _ViewDetailsState extends State<ViewDetails> {
-
   dynamic mentorFirestoreDB;
-
-  // Future<void> _onPressed() async {
-  //   mentorFirestoreDB =
-  //       FirebaseFirestore.instance.collection('Faculty').doc(widget.userID);
-  //   await mentorFirestoreDB.get().then((document) {
-  //     mentorName = document.data()["F_Name"];
-  //     mentorContactNumber = document.data()["F_ContactNumber"];
-  //   });
-  //   print("Mentor name is: " + mentorName);
-  //   print("Mentor contact no is: " + mentorContactNumber);
-  // }
 
   @override
   Widget build(BuildContext context) {
-    //_onPressed();
     final studentName = widget.reqDoc.get("S_Name");
     final courseName = widget.reqDoc.get("S_Course");
     final regNo = widget.reqDoc.get("S_RegNo");
@@ -127,29 +130,35 @@ class _ViewDetailsState extends State<ViewDetails> {
                   padding: const EdgeInsets.all(8.0),
                   child: ListTile(
                     title: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
                         Row(
                           children: [
-                            Text('Student Name: ',
+                            Text(
+                              'Student Name: ',
                               // style: darkSmallTextBold,
                             ),
-                            Text('$studentName',),
+                            Text(
+                              '$studentName',
+                            ),
                           ],
                         ),
                         Row(
                           children: [
-                            Text('Registration Number: ',
+                            Text(
+                              'Registration Number: ',
                               // style: darkSmallTextBold,
                             ),
-                            Text('$regNo', ),
+                            Text(
+                              '$regNo',
+                            ),
                           ],
                         ),
                         Row(
                           children: [
                             Text('Course Name: '
-                              // style: darkSmallTextBold,
-                            ),
+                                // style: darkSmallTextBold,
+                                ),
                             Text('$courseName'),
                           ],
                         ),
@@ -161,30 +170,35 @@ class _ViewDetailsState extends State<ViewDetails> {
                       children: [
                         GestureDetector(
                             child: MaterialButton(
-                              onPressed: () {
+                          onPressed: () {
+                            print("User ADDED");
 
-                                print("User ADDED");
+                            print(widget.userID);
+                            print(widget.groupName);
 
-                                print(widget.userID);
-                                print(widget.groupName);
-                                setState(() {
-                                  FirebaseFirestore.instance
-                                      .collection('Faculty')
-                                      .doc(widget.userID).collection('QuizGroup').doc(widget.groupName)
-                                      .update({
-                                    "AllottedStudent": FieldValue.arrayUnion([userID])
-                                  });
-                                  print(userID);
-                                  // FirebaseFirestore.instance
-                                  //     .collection('Student')
-                                  //     .doc(userID)
-                                  //     .update({
-                                  //
-                                  // });
-                                });
-                              },
-                              child: Icon(Icons.group_add),
-                            )),
+                            print("Hello: " + widget.docRef.toString());
+                            setState(() {
+                              FirebaseFirestore.instance
+                                  .collection('Faculty')
+                                  .doc(widget.userID)
+                                  .collection('QuizGroup')
+                                  .doc(widget.groupName)
+                                  .update({
+                                "AllottedStudent":
+                                    FieldValue.arrayUnion([userID])
+                              });
+                              print(userID);
+                              FirebaseFirestore.instance
+                                  .collection('Student')
+                                  .doc(userID)
+                                  .update({
+                                "GroupAdded": FieldValue.arrayUnion(
+                                    [widget.docRef.toString()])
+                              });
+                            });
+                          },
+                          child: Icon(Icons.group_add),
+                        )),
                         Text("Add User")
                       ],
                     ),
